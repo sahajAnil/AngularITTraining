4fcecf21ba82d33c18f2c397af1362b1df0fd5ed
Anil reddy3452 Reddy <sahajanil3452@gmail.com>
2023-01-04 17:52:44 +0530

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CourseComponent } from './components/course/course.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { ShopComponent } from './components/shop/shop.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'', redirectTo:'/home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'course',component:CourseComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'login',component:LoginComponent},
+  {path:'pages',component:PagesComponent},
+  {path:'shop',component:ShopComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CourseComponent } from './components/course/course.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { LoginComponent } from './components/login/login.component';
+import { PagesComponent } from './components/pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CourseComponent,
+    PagesComponent,
+    BlogComponent,
+    ShopComponent,
+    ContactComponent,
+    AboutComponent,
+    GalleryComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
